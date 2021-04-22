097de51930c70a85465a3ae453a7e0cf1b299496
Marcin Chodorek <marcin.chodorek3@gmail.com>
2021-04-22 16:03:00 +0200

    fixed visual bugs on landing page

--- scripts/cart.js
+++ scripts/cart.js
@@ -4,15 +4,19 @@ const selectList = document.querySelector('form select');
 const productsContainer = document.querySelector('.items-wrapper');
 
 const displayProduct = (data, sortType = '') => {
-    sortType != '' ?
-        sortType === 'highest' ? data.sort((a, b) => b.price - a.price) :
-            (sortType === 'lowest' ? data.sort((a, b) => a.price - b.price) :
-                (sortType === 'asc' ? data.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)) :
-                    data.sort((a, b) => (a.name > b.name) ? -1 : ((b.name > a.name) ? 1 : 0)))) : '';
+  sortType != ''
+    ? sortType === 'highest'
+      ? data.sort((a, b) => b.price - a.price)
+      : sortType === 'lowest'
+      ? data.sort((a, b) => a.price - b.price)
+      : sortType === 'asc'
+      ? data.sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0))
+      : data.sort((a, b) => (a.name > b.name ? -1 : b.name > a.name ? 1 : 0))
+    : '';
 
-    productsContainer.innerHTML = '';
-    data.forEach(product => {
-        productsContainer.innerHTML += `
+  productsContainer.innerHTML = '';
+  data.forEach((product) => {
+    productsContainer.innerHTML += `
         <div class="product-item">
             <div class="item-info">
                 <img src="${product.picture}" class="pimg">
@@ -21,49 +25,46 @@ const displayProduct = (data, sortType = '') => {
             </div>
             <div class="item-content">
                 <div class="show-info">${product.info}</div>
-                <div><span class="pprice">${product.price}</span>zł</div>
+                <div><span class="pprice">${product.price}</span>zł / szt.</div>
                 <button class="add-to-cart-btn"><i class="fas fa-cart-plus"></i></button>
             </div>
         </div>
         `;
-    });
+  });
 };
 
 const nameSearch = (search) => {
-    const productName = document.querySelectorAll('.item-info span');
-    Array.from(productName).filter(item => !item.textContent.toLowerCase().includes(search)).forEach(item => item.parentNode.parentNode.classList.add('filtered'));
-    Array.from(productName).filter(item => item.textContent.toLowerCase().includes(search)).forEach(item => item.parentNode.parentNode.classList.remove('filtered'));
-}
+  const productName = document.querySelectorAll('.item-info span');
+  Array.from(productName)
+    .filter((item) => !item.textContent.toLowerCase().includes(search))
+    .forEach((item) => item.parentNode.parentNode.classList.add('filtered'));
+  Array.from(productName)
+    .filter((item) => item.textContent.toLowerCase().includes(search))
+    .forEach((item) => item.parentNode.parentNode.classList.remove('filtered'));
+};
 
 searchField.addEventListener('keyup', () => {
-    const search = searchField.value.toLowerCase().trim();
-    nameSearch(search);
+  const search = searchField.value.toLowerCase().trim();
+  nameSearch(search);
 });
 
 const getProductList = async () => {
-    const productQuery = `/json/products.json`;
-    const request = await fetch(productQuery);
-    const productData = await request.json();
+  const productQuery = `/json/products.json`;
+  const request = await fetch(productQuery);
+  const productData = await request.json();
 
-    return productData;
+  return productData;
 };
 
-getProductList().then(data => {
-    displayProduct(data);
+getProductList().then((data) => {
+  displayProduct(data);
+  buttonsMessage();
+  getProductDetails();
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const selectValue = selectList.options[selectList.selectedIndex].value;
+    displayProduct(data, selectValue);
     buttonsMessage();
     getProductDetails();
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const selectValue = selectList.options[selectList.selectedIndex].value;
-        displayProduct(data, selectValue);
-        buttonsMessage();
-        getProductDetails();
-    })
+  });
 });
-
-
-
-
-
-
-
--- scripts/summary.js
+++ scripts/summary.js
@@ -1,4 +1,7 @@
 finButton.disabled = true;
+const firstName = document.querySelector('.order-form input[name="fname"]');
+const surname = document.querySelector('.order-form input[name="sname"]');
+
 orderButton.addEventListener('click', (e) => {
   e.preventDefault();
   const cartSummary = document.querySelector('.summary-wrapper');
@@ -10,6 +13,7 @@ closeButton.addEventListener('click', (e) => {
   const cartSummary = document.querySelector('.summary-wrapper');
   cartSummary.classList.add('removed');
 });
+
 finButton.addEventListener('click', (e) => {
   e.preventDefault();
   const orderMessage = document.querySelector('.summary-content');
@@ -19,10 +23,6 @@ finButton.addEventListener('click', (e) => {
   );
   let currentButton;
   const totalPrice = document.querySelector('.total');
-  const firstName = document.querySelector('.order-form input[name="fname"]')
-    .value;
-  const surname = document.querySelector('.order-form input[name="sname"]')
-    .value;
   const address = emailInput.value;
   let amount = 0;
   console.log(surname, firstName);
@@ -42,7 +42,7 @@ finButton.addEventListener('click', (e) => {
                   <html>
                       <h2>Ordered items: ${amount}</h2>
                       </br>
-                      <span>Buyer: ${firstName}  ${surname}</span>
+                      <span>Buyer: ${firstName.value}  ${surname.value}</span>
                       </br>
                       <span>Price: ${totalPrice.textContent}</span>
                       </br>
@@ -65,8 +65,48 @@ finButton.addEventListener('click', (e) => {
   );
 });
 
+firstName.addEventListener('keyup', () => {
+  if (
+    validateEmail(e.target.value.trim()) &&
+    firstName.value !== '' &&
+    surname.value !== ''
+  ) {
+    finButton.classList.remove('inactive');
+    finButton.disabled = false;
+    emailInput.classList.remove('invalid');
+    emailInput.classList.add('valid');
+  } else {
+    emailInput.classList.remove('valid');
+    emailInput.classList.add('invalid');
+    finButton.disabled = true;
+    finButton.classList.add('inactive');
+  }
+});
+
+surname.addEventListener('keyup', () => {
+  if (
+    validateEmail(e.target.value.trim()) &&
+    firstName.value !== '' &&
+    surname.value !== ''
+  ) {
+    finButton.classList.remove('inactive');
+    finButton.disabled = false;
+    emailInput.classList.remove('invalid');
+    emailInput.classList.add('valid');
+  } else {
+    emailInput.classList.remove('valid');
+    emailInput.classList.add('invalid');
+    finButton.disabled = true;
+    finButton.classList.add('inactive');
+  }
+});
+
 emailInput.addEventListener('keyup', (e) => {
-  if (validateEmail(e.target.value.trim())) {
+  if (
+    validateEmail(e.target.value.trim()) &&
+    firstName.value !== '' &&
+    surname.value !== ''
+  ) {
     finButton.classList.remove('inactive');
     finButton.disabled = false;
     emailInput.classList.remove('invalid');
